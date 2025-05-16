774be54e9c6fc14a533c924e17a108b26d235c0a
Marzmans <marzan.mk@gmail.com>
2025-05-16 14:31:57 -0400

    vercel & organization

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ require('dotenv').config();
 const apiRoutes = require('./api');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('.')); 
+app.use(express.static('public')); 
 
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+
+module.exports = app;
